22a708ec94bec28551ed86ecf62aa69594fc0dbd
Timothy Hayes <TimHayes95@gmail.com>
2025-07-22 14:06:32 +0200

    Placeholder image for landing page

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -7,7 +7,7 @@ export default function Landing() {
   return (
      <div className={styles.landing}>
       <div className={styles.overlay}>
-        <h1 className={styles.title}>Welcome to My Portfolio</h1>
+        <h1 className={styles.title}>Timothy Hayes | Interactive Resume</h1>
       </div>
     </div>
   );
