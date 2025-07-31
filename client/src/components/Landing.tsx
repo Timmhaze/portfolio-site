09b5ec3259da45259ce1743ea2190b1b0faa19fa
Timothy Hayes <TimHayes95@gmail.com>
2025-07-31 14:54:23 +0200

    landing page placeholder note

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -6,6 +6,7 @@ export default function Landing() {
      <div className={styles.landing}>
       <div className={styles.overlay}>
         <h1 className={styles.title}>Timothy Hayes | Interactive Resume</h1>
+        <h6 className={styles.subtitle}>this landing is placeholder :P</h6>
       </div>
     </div>
   );
