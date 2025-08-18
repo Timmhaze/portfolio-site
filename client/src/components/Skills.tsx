2e1a082da93bdfa7ec248243736953977b66c552
Timothy Hayes <TimHayes95@gmail.com>
2025-08-18 12:56:46 +0200

    Fixed null values for Skills section margins and structure

--- client/src/components/Skills.tsx
+++ client/src/components/Skills.tsx
@@ -22,14 +22,14 @@ export default function Skills() {
     <Container className={styles['skills-container']}>
       <Row>
         <Col>
-          <p className={styles['skill-level-desc']}>
+          <div className={styles['skill-level-desc']}>
             Self-assessment can be tricky - rate too high and it sounds arrogant; too low and it sells you short. This section reflects an honest self-evaluation based on real-world experience and personal confidence.
             To clarify my scale: one star means "I’m familiar with this," while five stars means "I’m highly capable, use this daily, and could confidently teach others." Hover or tap a skill to see more detail.
-          </p>
+          </div>
         </Col>
       </Row>
       <Row className={styles['skill-list-container-row']}>
-        <Col md={5} className={styles['skills-left-col']}>
+        <Col className={styles['skills-left-col']}>
           <Row className={styles['skills-title-row']}>
             <motion.h4 className={styles['skills-title']} variants={slideInFromTop} {...defaultMotionHelper}>Hard Skills</motion.h4>
             <hr className={styles['skills-hr']}/>
